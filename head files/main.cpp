f6cf3571354d83b6e83b5d589eb7ead5cf52aaa7
ZWFolirly <32008078+ZWFolirly@users.noreply.github.com>
2018-07-01 16:09:26 +0800

    Add files via upload

---
+++ head files/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <utility>
+#include <chrono>
+#include <functional>
+#include <string>
+#include <cuckoohash_map.hh>
+#include <cmath>
+#include <fstream>
+#include <ctime>
+#include <map>
+#include <hash_map>
+#include <thread>
+#include <mutex>
+
+#include "hash.h"
+#include "test.h"
+using namespace std;
+using namespace __gnu_cxx;
+
+bool version_count[LIBCUCKOO_DEFAULT_SIZE/LIBCUCKOO_DEFAULT_SLOT_PER_BUCKET]{false};
+int main() {
+    //runCuckoohash();//non-concurrency
+    //runLockConcurrentCuckoohash();
+    //runNonlockConcurrentCuckoohash();
+    concurrent_load();
+    return 0;
+}
